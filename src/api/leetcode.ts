306beb443aeda631cdfb1bc7ff866deb8766e7f3
Григорий Гаврилов <lowww1337@mail.ru>
2023-06-15 14:53:13 +0300

    #184 <Fix> Добавит правки
    close #184

--- src/api/leetcode.ts
+++ src/api/leetcode.ts
@@ -2,13 +2,21 @@ import { DetailProblem, Problem } from "$/entity/leetCode";
 
 const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST;
 export async function leetcodeProblems(): Promise<Problem[]> {
-  const response = await fetch(`${BACKEND_HOST}leetcode`);
+  const response = await fetch(`${BACKEND_HOST}leetcode`, {
+    headers: {
+      Authorization: "Bearer " + location.href,
+    },
+  });
   return response.json();
 }
 
 export async function leetcodeDetailProblems(
   slug: string
 ): Promise<DetailProblem> {
-  const response = await fetch(`${BACKEND_HOST}leetcode/` + slug);
+  const response = await fetch(`${BACKEND_HOST}leetcode/` + slug, {
+    headers: {
+      Authorization: "Bearer " + location.href,
+    },
+  });
   return response.json();
 }
