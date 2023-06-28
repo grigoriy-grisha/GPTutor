665f6a2602873e37582929af7b2a0ebb2f03617d
Григорий Гаврилов <lowww1337@mail.ru>
2023-06-28 18:48:19 +0300

    #351 <Feat> Поправит скачивание
    close #351

--- src/api/messages.ts
+++ src/api/messages.ts
@@ -24,5 +24,5 @@ export function getMessagesById(historyId: string): Promise<RemoteMessage[]> {
 }
 
 export function downloadMessagesUrl(type: string, historyId: string) {
-  return `${BACKEND_HOST}messages/${type}/${historyId}?vk_access_token_settings=menu&vk_app_id=51602327&vk_are_notifications_enabled=1&vk_is_app_user=1&vk_is_favorite=1&vk_language=ru&vk_platform=desktop_web&vk_ref=other&vk_ts=1687965397&vk_user_id=548334196&sign=etfd0Hbqu9v3Dq_ab-9lcyiNDJVuge4ppGYQ7M_BnjM`;
+  return `${BACKEND_HOST}messages/${type}/${historyId}${location.search}`;
 }
