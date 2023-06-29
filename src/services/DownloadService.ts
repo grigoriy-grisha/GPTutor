9541db2d4555fd34a0db70c2b128ce18183721aa
Григорий Гаврилов <lowww1337@mail.ru>
2023-06-29 11:13:30 +0300

    #351 <Feat> Поправит скачивание
    close #351

--- src/services/DownloadService.ts
+++ src/services/DownloadService.ts
@@ -43,24 +43,7 @@ class DownloadService {
   }
 
   async downloadByLink(link: string, filename: string) {
-    if (process.env.NODE_ENV === "development") {
-      this.downloadLink(link, filename);
-      return;
-    }
-
-    console.log(link);
-
-    await bridge
-      .send("VKWebAppDownloadFile", {
-        url: link,
-        filename: filename,
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((res) => {
-        console.log(res);
-      });
+    this.downloadLink(link, filename);
   }
 }
 
