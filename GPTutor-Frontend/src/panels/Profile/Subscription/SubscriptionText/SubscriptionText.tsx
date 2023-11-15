7ce8966168514a05fe48349e1fe827f8a21adf98
Grigoriy Gavrilov <lowww1337@mail.ru>
2023-11-16 02:43:19 +0300

    #428 <Feat> Доработает api ключей

--- GPTutor-Frontend/src/panels/Profile/Subscription/SubscriptionText/SubscriptionText.tsx
+++ GPTutor-Frontend/src/panels/Profile/Subscription/SubscriptionText/SubscriptionText.tsx
@@ -5,7 +5,7 @@ import { subscriptionsController } from "$/entity/subscriptions";
 function SubscriptionText() {
   return (
     <div>
-      <ul style={{ listStyle: "none" }}>
+      <ul style={{ listStyle: "none", paddingLeft: 0 }}>
         <li>Генерируйте сразу 4 фотографии от 10 секунд!</li>
         <li>Настраивайте размеры фотографий!</li>
         <li>Отключение любой рекламы!</li>
