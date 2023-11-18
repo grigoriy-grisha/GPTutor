55169d8bf2e442a8f1b09fcd0edd2290e78980e7
Grigoriy Gavrilov <lowww1337@mail.ru>
2023-11-19 00:17:07 +0300

    #428 <Feat> Доработает api ключей

--- GPTutor-Backend/src/main/java/com/chatgpt/WebConfig.java
+++ GPTutor-Backend/src/main/java/com/chatgpt/WebConfig.java
@@ -2,10 +2,10 @@ package com.chatgpt;
 
 import com.chatgpt.interceptors.AuthorizationInterceptor;
 import com.chatgpt.interceptors.CorsInterceptor;
+import com.chatgpt.interceptors.RateLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,10 +22,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private CorsInterceptor corsInterceptor;
 
+    @Autowired RateLimitInterceptor rateLimitInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(corsInterceptor);
         registry.addInterceptor(authorizationInterceptor);
+        registry.addInterceptor(rateLimitInterceptor);
     }
 }
 
---
+++ GPTutor-Backend/src/main/java/com/chatgpt/interceptors/RateLimitInterceptor.java
@@ -0,0 +1,61 @@
+package com.chatgpt.interceptors;
+
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@Component
+public class RateLimitInterceptor implements HandlerInterceptor {
+
+    private final Map<String, Map<String, Bucket>> urlBuckets = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        String userId = (String) request.getAttribute("vkUserId");
+
+        System.out.println(userId);
+
+        if (userId != null) {
+            Bucket bucket = getBucket(userId, uri);
+            if (bucket.tryConsume(1)) {
+                return true;
+            } else {
+                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                response.getWriter().write("Rate limit exceeded");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private synchronized Bucket getBucket(String userId, String uri) {
+        Map<String, Bucket> userSpecificBuckets = urlBuckets.computeIfAbsent(userId, k -> new ConcurrentHashMap<>());
+        return userSpecificBuckets.computeIfAbsent(uri, k -> Bucket.builder()
+                .addLimit(limit -> limit.capacity(getCapacityForUrl(uri)).refillGreedy(getCapacityForUrl(uri), Duration.ofMinutes(1)))
+                .build());
+    }
+
+    private int getCapacityForUrl(String uri) {
+
+        if (uri.startsWith("/images")) {
+            return 10;
+        }
+
+        if (uri.startsWith("/history")) {
+            return 30;
+        }
+
+        return 10;
+    }
+}
