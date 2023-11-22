dbf29423a3b77ed1ecafa166775578133a0f340d
Grigoriy Gavrilov <lowww1337@mail.ru>
2023-11-22 23:31:39 +0300

    #428 <Feat> Доработает api ключей

--- GPTutor-Backend/src/main/java/com/chatgpt/services/ConversationsService.java
+++ GPTutor-Backend/src/main/java/com/chatgpt/services/ConversationsService.java
@@ -45,7 +45,7 @@ public class ConversationsService {
 
         String input = mapper.writeValueAsString(chatGptRequest);
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.openai.com/v1/chat/completions"))
+                .uri(URI.create("https://api.aiguoguo199.com/v1/chat/completions"))
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + apiKey.getFirst().getKey())
                 .POST(HttpRequest.BodyPublishers.ofString(input))
