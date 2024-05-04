33d44901dd9ebe7dd7d2e5d5b76ccef6c0b862cf
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-05-05 02:04:20 +0300

    #514 Выкатит gpt-4 bing в платные модели
    close #514

--- GPTutor-Models/llm/models.py
+++ GPTutor-Models/llm/models.py
@@ -3,10 +3,64 @@ import sched
 import time
 
 from g4f.Provider import Bing, DeepInfra
-from g4f.models import blackbox, llama3_70b_instruct, llama3_8b_instruct, llama2_7b, llama2_13b, llama2_70b, \
-    dbrx_instruct, mixtral_8x22b, Model, claude_3_opus, pi
-from g4f.providers.retry_provider import RetryProvider
 from g4f.client import Client
+from g4f.models import blackbox, llama3_70b_instruct, llama3_8b_instruct, llama2_7b, llama2_13b, llama2_70b, \
+    dbrx_instruct, Model, claude_3_opus, pi
+from g4f.providers.retry_provider import RetryProvider
+
+wizardLM_2_8x22B = Model(
+    name="microsoft/WizardLM-2-8x22B",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+wizardLM_2_7B = Model(
+    name="microsoft/WizardLM-2-7B",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+gemma_1_1_7b = Model(
+    name="google/gemma-1.1-7b-it",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+mixtral_8x7B = Model(
+    name="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+mistral_7B = Model(
+    name="mistralai/Mistral-7B-Instruct-v0.2",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+dolphin_26_mixtral_8x7b = Model(
+    name="cognitivecomputations/dolphin-2.6-mixtral-8x7b",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+lzlv_70b_fp16_hf = Model(
+    name="lizpreciatior/lzlv_70b_fp16_hf",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+openchat_35 = Model(
+    name="openchat/openchat_3.5",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
+
+airoboros_70b = Model(
+    name="deepinfra/airoboros-70b",
+    base_provider="huggingface",
+    best_provider=RetryProvider([DeepInfra])
+)
 
 models_dict = {
     "blackbox": {
@@ -75,6 +129,7 @@ models = [
         "lang": "Имеется поддержка Русского языка",
         "active": True,
     },
+
     {
         "model": "llama3_70b",
         "description":
@@ -82,6 +137,76 @@ models = [
         "lang": "Имеется частичная поддержка Русского языка",
         "active": True,
     },
+    {
+        "model": "airoboros_70b",
+        "description": "Аналог GPT-4. Улучшенная версия Llama2-70b",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "wizardLM_2_8x22B",
+        "description":
+            "Аналог GPT-3.5. Модель от microsoft. Хорошо подходит для обычного общения и соченения историй",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "wizardLM_2_8x22B",
+        "description":
+            "Аналог GPT-3.5. Модель от microsoft. Хорошо подходит для обычного общения и соченения историй",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "wizardLM_2_8x22B",
+        "description":
+            "Слабее GPT-3.5. Модель от microsoft. Хорошо подходит для обычного общения и соченения историй, работает очень быстро",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "gemma_1_1_7b",
+        "description":
+            "Аналог GPT-3.5. Модель от goggle. Хорошо подходит для программирования и обычного общения",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "mixtral_8x7B",
+        "description":
+            "Аналог чем GPT-3.5. Хорошо подходит для программирования, работает быстро",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "mistral_7B",
+        "description":
+            "Аналог чем GPT-3.5. Хорошо подходит для программирования, работает быстро",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "dolphin_26_mixtral_8x7b",
+        "description":
+            "Аналог GPT-3.5. Модель заточенная специально под программирование",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "lzlv_70b_fp16_hf",
+        "description":
+            "Аналог GPT-3.5. Креативная модель, хорошо подходит для развлечения и создания контента",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+    {
+        "model": "openchat_35",
+        "description":
+            "Полный аналог gpt-3.5",
+        "lang": "Имеется поддержка Русского языка",
+        "active": True,
+    },
+
     {
         "model": "mixtral_8x22b",
         "description": "Аналог GPT-3.5, Во многих аспектах превосходит GPT-3.5. Обучена до 2024 года",
