755932a238ca1adfdbb2eecd18f781a67981c577
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-05-07 23:47:05 +0300

    #514 Выкатит gpt-4 bing в платные модели
    close #514

--- GPTutor-Frontend/src/entity/image/styles.ts
+++ GPTutor-Frontend/src/entity/image/styles.ts
@@ -73,89 +73,256 @@ export const styles = [
 
 export const models = [
   {
-    value: "sd",
-    label: "Stable Diffusion 1.5",
-  },
-
-  {
-    value: "f222-diffusion",
-    label: "F222 Diffusion",
+    value: "3Guofeng3_v34.safetensors [50f420de]",
+    label: "3 Guofeng3 V3.4",
   },
   {
-    value: "dream-shaper-8797",
-    label: "Dream Shaper 8797",
+    value: "absolutereality_V16.safetensors [37db0fc3]",
+    label: "Absolute Reality V1.6",
   },
   {
-    value: "realistic-vision-v13",
-    label: "Realistic Vision V13",
+    value: "absolutereality_v181.safetensors [3d9d4d2b]",
+    label: "Absolute Reality V1.8.1",
   },
   {
-    value: "anything-v5",
-    label: "Anything V5",
+    value: "amIReal_V41.safetensors [0a8a2e61]",
+    label: "Am I Real V4.1",
   },
   {
-    value: "anything-v3",
+    value: "analog-diffusion-1.0.ckpt [9ca13f02]",
+    label: "Analog V1",
+  },
+  {
+    value: "anythingv3_0-pruned.ckpt [2700c435]",
     label: "Anything V3",
   },
   {
-    value: "anything-v4",
-    label: "Anything V4",
+    value: "anything-v4.5-pruned.ckpt [65745d25]",
+    label: "Anything V4.5",
   },
   {
-    value: "realistic-vision-v40",
-    label: "Realistic Vision",
+    value: "anythingV5_PrtRE.safetensors [893e49b9]",
+    label: "Anything V5",
   },
   {
-    value: "samaritan-3d-cartoon",
-    label: "Samaritan 3D Cartoon",
+    value: "AOM3A3_orangemixs.safetensors [9600da17]",
+    label: "AbyssOrangeMix V3",
   },
   {
-    value: "real-cartoon-realist",
-    label: "Real Cartoon Realist",
+    value: "blazing_drive_v10g.safetensors [ca1c1eab]",
+    label: "Blazing Drive V10g",
   },
   {
-    value: "food-crit",
-    label: "Food Crit",
+    value: "breakdomain_I2428.safetensors [43cc7d2f]",
+    label: "BreakDomain I2428",
   },
   {
-    value: "lowpoly-diffusion",
-    label: "Lowpoly Diffusion",
+    value: "breakdomain_M2150.safetensors [15f7afca]",
+    label: "BreakDomain M2150",
   },
   {
-    value: "pixel-sprite",
-    label: "Pixel Sprite",
+    value: "cetusMix_Version35.safetensors [de2f2560]",
+    label: "CetusMix Version35",
   },
   {
-    value: "mix-appfactory",
-    label: "Mix Appfactory",
+    value: "childrensStories_v13D.safetensors [9dfaabcb]",
+    label: "Children's Stories V1 3D",
   },
   {
-    value: "inkmix",
-    label: "Inkmix",
+    value: "childrensStories_v1SemiReal.safetensors [a1c56dbb]",
+    label: "Children's Stories V1 SemiReal",
   },
   {
-    value: "vector-art",
-    label: "Vector Art",
+    value: "childrensStories_v1ToonAnime.safetensors [2ec7b88b]",
+    label: "Children's Stories V1 Toon-Anime",
   },
   {
-    value: "synthwave-diffusion",
-    label: "Synthwave Diffusion",
+    value: "Counterfeit_v30.safetensors [9e2a8f19]",
+    label: "Counterfeit V3.0",
   },
   {
-    value: "midjourney-papercut",
-    label: "Midjourney Papercut",
+    value: "cuteyukimixAdorable_midchapter3.safetensors [04bdffe6]",
+    label: "CuteYukimix MidChapter3",
   },
   {
-    value: "icons-diffusion",
-    label: "Icons Diffusion",
+    value: "cyberrealistic_v33.safetensors [82b0d085]",
+    label: "CyberRealistic V3.3",
   },
   {
-    value: "disney-pixar-cartoon",
-    label: "Disney Pixar Cartoon",
+    value: "dalcefo_v4.safetensors [425952fe]",
+    label: "Dalcefo V4",
   },
   {
-    value: "woolitize-diffusion",
-    label: "Woolitize Diffusion",
+    value: "deliberate_v2.safetensors [10ec4b29]",
+    label: "Deliberate V2",
+  },
+  {
+    value: "deliberate_v3.safetensors [afd9d2d4]",
+    label: "Deliberate V3",
+  },
+  {
+    value: "dreamlike-anime-1.0.safetensors [4520e090]",
+    label: "Dreamlike Anime V1",
+  },
+  {
+    value: "dreamlike-diffusion-1.0.safetensors [5c9fd6e0]",
+    label: "Dreamlike Diffusion V1",
+  },
+  {
+    value: "dreamlike-photoreal-2.0.safetensors [fdcf65e7]",
+    label: "Dreamlike Photoreal V2",
+  },
+  {
+    value: "dreamshaper_6BakedVae.safetensors [114c8abb]",
+    label: "Dreamshaper 6 baked vae",
+  },
+  {
+    value: "dreamshaper_7.safetensors [5cf5ae06]",
+    label: "Dreamshaper 7",
+  },
+  {
+    value: "dreamshaper_8.safetensors [9d40847d]",
+    label: "Dreamshaper 8",
+  },
+  {
+    value: "edgeOfRealism_eorV20.safetensors [3ed5de15]",
+    label: "Edge of Realism EOR V2.0",
+  },
+  {
+    value: "EimisAnimeDiffusion_V1.ckpt [4f828a15]",
+    label: "Eimis Anime Diffusion V1.0",
+  },
+  {
+    value: "elldreths-vivid-mix.safetensors [342d9d26]",
+    label: "Elldreth's Vivid",
+  },
+  {
+    value: "epicphotogasm_xPlusPlus.safetensors [1a8f6d35]",
+    label: "epiCPhotoGasm X Plus Plus",
+  },
+  {
+    value: "epicrealism_naturalSinRC1VAE.safetensors [90a4c676]",
+    label: "EpiCRealism Natural Sin RC1",
+  },
+  {
+    value: "epicrealism_pureEvolutionV3.safetensors [42c8440c]",
+    label: "EpiCRealism Pure Evolution V3",
+  },
+  {
+    value: "ICantBelieveItsNotPhotography_seco.safetensors [4e7a3dfd]",
+    label: "I Cant Believe Its Not Photography Seco",
+  },
+  {
+    value: "indigoFurryMix_v75Hybrid.safetensors [91208cbb]",
+    label: "Indigo Furry Mix V7.5 Hybrid",
+  },
+  {
+    value: "juggernaut_aftermath.safetensors [5e20c455]",
+    label: "Juggernaut Aftermath",
+  },
+  {
+    value: "lofi_v4.safetensors [ccc204d6]",
+    label: "Lofi V4",
+  },
+  {
+    value: "lyriel_v16.safetensors [68fceea2]",
+    label: "Lyriel V1.6",
+  },
+  {
+    value: "majicmixRealistic_v4.safetensors [29d0de58]",
+    label: "MajicMix Realistic V4",
+  },
+  {
+    value: "mechamix_v10.safetensors [ee685731]",
+    label: "MechaMix V1.0",
+  },
+  {
+    value: "meinamix_meinaV9.safetensors [2ec66ab0]",
+    label: "MeinaMix Meina V9",
+  },
+  {
+    value: "meinamix_meinaV11.safetensors [b56ce717]",
+    label: "MeinaMix Meina V11",
+  },
+  {
+    value: "neverendingDream_v122.safetensors [f964ceeb]",
+    label: "Neverending Dream V1.22",
+  },
+  {
+    value: "openjourney_V4.ckpt [ca2f377f]",
+    label: "Openjourney V4",
+  },
+  {
+    value: "pastelMixStylizedAnime_pruned_fp16.safetensors [793a26e8]",
+    label: "Pastel-Mix",
+  },
+  {
+    value: "portraitplus_V1.0.safetensors [1400e684]",
+    label: "Portrait+ V1",
+  },
+  {
+    value: "protogenx34.safetensors [5896f8d5]",
+    label: "Protogen x3.4",
+  },
+  {
+    value: "Realistic_Vision_V1.4-pruned-fp16.safetensors [8d21810b]",
+    label: "Realistic Vision V1.4",
+  },
+  {
+    value: "Realistic_Vision_V2.0.safetensors [79587710]",
+    label: "Realistic Vision V2.0",
+  },
+  {
+    value: "Realistic_Vision_V4.0.safetensors [29a7afaa]",
+    label: "Realistic Vision V4.0",
+  },
+  {
+    value: "Realistic_Vision_V5.0.safetensors [614d1063]",
+    label: "Realistic Vision V5.0",
+  },
+  {
+    value: "redshift_diffusion-V10.safetensors [1400e684]",
+    label: "Redshift Diffusion V1.0",
+  },
+  {
+    value: "revAnimated_v122.safetensors [3f4fefd9]",
+    label: "ReV Animated V1.2.2",
+  },
+  {
+    value: "rundiffusionFX25D_v10.safetensors [cd12b0ee]",
+    label: "RunDiffusion FX 2.5D V1.0",
+  },
+  {
+    value: "rundiffusionFX_v10.safetensors [cd4e694d]",
+    label: "RunDiffusion FX Photorealistic V1.0",
+  },
+  {
+    value: "sdv1_4.ckpt [7460a6fa]",
+    label: "SD V1.4",
+  },
+  {
+    value: "v1-5-pruned-emaonly.safetensors [d7049739]",
+    label: "SD V1.5",
+  },
+  {
+    value: "v1-5-inpainting.safetensors [21c7ab71]",
+    label: "SD V1.5 Inpainting",
+  },
+  {
+    value: "shoninsBeautiful_v10.safetensors [25d8c546]",
+    label: "Shonin's Beautiful People V1.0",
+  },
+  {
+    value: "theallys-mix-ii-churned.safetensors [5d9225a4]",
+    label: "TheAlly's Mix II",
+  },
+  {
+    value: "timeless-1.0.ckpt [7c4971d4]",
+    label: "Timeless V1",
+  },
+  {
+    value: "toonyou_beta6.safetensors [980f6b15]",
+    label: "ToonYou Beta 6",
   },
 ];
 
@@ -164,67 +331,31 @@ export function getModelByValue(value: string) {
 }
 
 export const samplers = [
-  { value: "DDPMScheduler", label: "DDPMScheduler" },
-  {
-    value: "Euler a",
-    label: "Euler a",
-  },
   {
     value: "Euler",
     label: "Euler",
   },
   {
-    value: "DPM++ 2M",
-    label: "DPM++ 2M",
-  },
-  {
-    value: "DPM++ 2M Karras",
-    label: "DPM++ 2M Karras",
-  },
-  {
-    value: "DPM++ 2M SDE",
-    label: "DPM++ 2M SDE",
-  },
-  {
-    value: "DPM++ 2M SDE Karras",
-    label: "DPM++ 2M SDE Karras",
-  },
-  {
-    value: "DPM++ SDE",
-    label: "DPM++ SDE",
-  },
-  {
-    value: "DPM++ SDE Karras",
-    label: "DPM++ SDE Karras",
-  },
-  {
-    value: "DPM2",
-    label: "DPM2",
-  },
-  {
-    value: "DPM2 Karras",
-    label: "DPM2 Karras",
-  },
-  {
-    value: "DPM2 a",
-    label: "DPM2 a",
-  },
-  {
-    value: "DPM2 a Karras",
-    label: "DPM2 a Karras",
+    value: "Euler a",
+    label: "Euler a",
   },
   {
     value: "Heun",
     label: "Heun",
   },
   {
-    value: "LMS",
-    label: "LMS",
+    value: "DPM++ 2M Karras",
+    label: "DPM++ 2M Karras",
   },
   {
-    value: "LMS Karras",
-    label: "LMS Karras",
+    value: "DPM++ SDE Karras",
+    label: "DPM++ SDE Karras",
+  },
+  {
+    value: "DDIM",
+    label: "DDIM",
   },
 ];
+
 export const defaultModel = styles[0].value;
 export const defaultSampler = samplers[0].value;
