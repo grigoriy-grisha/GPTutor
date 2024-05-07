3b65fc3db81b2077d969ab5e84379c7fbc5086f3
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-05-07 23:49:00 +0300

    #514 Выкатит gpt-4 bing в платные модели
    close #514

--- GPTutor-Models/app.py
+++ GPTutor-Models/app.py
@@ -4,7 +4,8 @@ from flask import Flask, Response, request
 
 from images.prodia import txt2img
 from llm.index import create_completions
-from llm.models import models
+from llm.models import models, run_check_models
+from vk.bot import run_long_pool
 
 app = Flask(__name__)
 
@@ -46,9 +47,9 @@ def run_flask():
 
 if __name__ == '__main__':
     flask_thread = threading.Thread(target=run_flask)
-    # check_models_thread = threading.Thread(target=run_check_models)
-    # run_long_pool_thread = threading.Thread(target=run_long_pool)
+    check_models_thread = threading.Thread(target=run_check_models)
+    run_long_pool_thread = threading.Thread(target=run_long_pool)
 
     flask_thread.start()
-    # check_models_thread.start()
-    # run_long_pool_thread.start()
+    check_models_thread.start()
+    run_long_pool_thread.start()
