d6a593ca7b0c077c01e46fc38dc9bd41ed76cecd
Gavrilov Grigoriy <lowww1337@gmail.com>
2024-06-06 11:35:31 +0300

    Пофиксит прокси

--- GPTutor-Models/llm/proxy.py
+++ GPTutor-Models/llm/proxy.py
@@ -1,7 +1,12 @@
 import random
 
-proxies = []
+proxies = [
+    "http://dXo2w0:Y7jJDd@38.170.124.149:9016",
+    "http://dXo2w0:Y7jJDd@38.170.121.94:9521",
+    "http://dXo2w0:Y7jJDd@38.170.121.148:9651",
+    "http://dXo2w0:Y7jJDd@138.219.122.242:9388",
+]
 
 
 def get_proxy():
-    return None
+    return random.choice(proxies)
