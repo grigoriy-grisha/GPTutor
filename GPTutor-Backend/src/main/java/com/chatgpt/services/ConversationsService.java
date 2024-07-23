0fa2f53535b62bbf90fa19fd7c5ba7a2e08a0bfc
Grigoriy Gavrilov <lowww1337@mail.ru>
2024-07-24 01:00:16 +0300

    #558 Добавит новые модели

--- GPTutor-Backend/src/main/java/com/chatgpt/services/ConversationsService.java
+++ GPTutor-Backend/src/main/java/com/chatgpt/services/ConversationsService.java
@@ -3,12 +3,18 @@ package com.chatgpt.services;
 import com.chatgpt.entity.ApiKey;
 import com.chatgpt.entity.ChatGptRequest;
 import com.chatgpt.entity.ConversationRequest;
+import com.chatgpt.entity.requests.QuestionRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.util.Pair;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
